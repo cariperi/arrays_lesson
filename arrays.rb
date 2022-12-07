c8b70f6ad057ecf611a424cda5e986d8ad8058c2
Caroline Peri <caroline.anne.peri@gmail.com>
2022-12-06 21:17:16 -0500

    Add code to call methods on arrays and explain actions

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,19 @@ strings_array = [ 'cat', 'dog', 'horse', 'rabbit']
 integers_array = [1, 2, 3, 4]
 floats_array = [1.0, 2.4, 3.7, 4.6]
 booleans_array = [true, true, true, false]
+
+# Calling the pop method will remove the last element 'rabbit' from the strings_array, and will return the 'rabbit' value.
+strings_array.pop
+# p strings_array.pop
+
+# Calling the push method will add the given elements (5, 6) to the end of the integers_array, and will return the new, larger array.
+integers_array.push(5, 6)
+# p integers_array.push(5, 6)
+
+# Calling the shift method will remove the first element '1.0' from the floats_array, and will return the '1.0' value.
+floats_array.shift
+# p floats_array.shift
+
+# Calling the unshift method will add the specific argument value (false) to the beginning of the booleans_array, and will return the new, larger array.
+booleans_array.unshift(false)
+# p booleans_array.unshift(false)
